b0df815b625c0f1ec5fd57d1f65eab5e2ea972ff
Arashikage-XuanHoang <hoangdh2392005@gmail.com>
2026-03-09 20:41:31 +0700

    First Commit

---
+++ code.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+# tạo mảng numpy
+a = np.array([1, 2, 3, 4, 5])
+
+print("Array:", a)
+
+# tính tổng
+print("Sum:", np.sum(a))
+
+# tính trung bình
+print("Mean:", np.mean(a))
+
+# nhân toàn bộ phần tử với 2
+print("Multiply by 2:", a * 2)
