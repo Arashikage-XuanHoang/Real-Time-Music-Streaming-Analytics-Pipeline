1b60d2ddb33bd8656c0db4982ec27be8ed5d06e0
Arashikage-XuanHoang <hoangdh2392005@gmail.com>
2026-03-09 20:46:59 +0700

    Add feature

--- code.py
+++ code.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 # tạo mảng numpy
 a = np.array([1, 2, 3, 4, 5])
+b = ["Toi", "ten", "la"]
+
 
 print("Array:", a)
 
@@ -14,3 +16,6 @@ print("Mean:", np.mean(a))
 
 # nhân toàn bộ phần tử với 2
 print("Multiply by 2:", a * 2)
+
+for i in range(len(b)):
+    print(b[i] + str("Haha"))
