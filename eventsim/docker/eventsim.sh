07a1d907fc7a6b6a625f0af4998988b81eea4261
Arashikage-XuanHoang <hoangdh2392005@gmail.com>
2026-03-13 14:51:58 +0700

    13_3 commit

--- eventsim/docker/eventsim.sh
+++ eventsim/docker/eventsim.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-java -XX:+AggressiveOpts \
-     -XX:+UseG1GC \
-     -XX:+UseStringDeduplication \
-     -Xmx8G \
-     -jar /opt/eventsim/eventsim-assembly-2.0.jar "$@"
+#! /bin/bash
+java -XX:+UseG1GC -XX:+UseStringDeduplication -Xms128M -Xmx256M -jar eventsim-assembly-2.0.jar $*
