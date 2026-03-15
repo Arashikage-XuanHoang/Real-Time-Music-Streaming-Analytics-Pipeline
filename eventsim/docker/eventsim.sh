1197719ea3426015b91645e48ff3bb8dff07df22
Arashikage-XuanHoang <hoangdh2392005@gmail.com>
2026-03-15 21:10:48 +0700

    lite_version

--- eventsim/docker/eventsim.sh
+++ eventsim/docker/eventsim.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -XX:+UseG1GC -XX:+UseStringDeduplication -Xms1g -Xmx3g -jar eventsim-assembly-2.0.jar $*
+java -XX:+UseG1GC -XX:+UseStringDeduplication -Xms256m -Xmx512m -jar eventsim-assembly-2.0.jar $*
